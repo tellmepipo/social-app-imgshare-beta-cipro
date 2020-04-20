fec7a1a86a1ffdc9f6b04babbb3f15498751e241
tellmepipo <jeanpierre.guevaraotero@gmail.com>
2020-04-20 00:14:36 +0000

    mandar informacion de imagenes subidas al servidor mongodb

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,6 +2,8 @@ const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
 
+const { Image } = require('../models/index');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
@@ -13,12 +15,17 @@ ctrl.create = async (req, res) => {
     console.log(imgUrl);
     const imageTempPath = req.file.path;
     const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve('src/public/upload/${imgUrl}${ext}');
+    const targetPath = path.resolve('src/public/upload/${imgUrl}${ext}')
 
     if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
         await fs.rename(imageTempPath, targetPath);
+        const newImg = new Image({
+            title: req.body.title,
+            filename: imgUrl + ext,
+            description: req.body.description
+        });
+        const imageSaved = await newImg.save();
     }
-
     res.send('works!');
 };
 
--- src/database.js
+++ src/database.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const { database } = require('./keys');
 
-mongoose.connect(database.URI)
+mongoose.connect(database.URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
