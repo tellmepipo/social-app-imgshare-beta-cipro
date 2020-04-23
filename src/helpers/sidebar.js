a3fa2bc466b977cb220c2718f30efe156375de15
tellmepipo <jeanpierre.guevaraotero@gmail.com>
2020-04-23 03:14:37 +0000

    casi terminado

--- src/helpers/sidebar.js
+++ src/helpers/sidebar.js
@@ -2,6 +2,18 @@ const Stats = require('./stats');
 const Images = require('./images');
 const Comments = require('./comments');
 
-module.exports = function (viewModel) {
-    Images.popular();
+module.exports = async viewModel => {
+    
+    const results = await Promise.all([
+        Stats(),
+        Images.popular(),
+        Comments.newest()
+    ]);
+    viewModel.sidebar = {
+        stats: results[0],
+        popular: results[1],
+        comments: results[2]
+    };
+
+    return viewModel;
 }
